970914ef4258b19dabc5cc4d3b8e99d0042704da
Sasa Nedic <svetasmak0@gmail.com>
2024-05-03 23:12:40 +0200

    Create Header component

---
+++ vite-project/src/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return(
+        <nav>
+            <ul>
+                <li>FAST REACT PIZZA CO.</li>
+                <li><input type="text" placeholder="Search Order  #"/></li>
+            </ul>
+        </nav>
+    )
+}
+export default Header;
