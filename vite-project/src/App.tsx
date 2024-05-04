eea43252d0173108eb4801747ca410bbb033ad9a
Sasa Nedic <svetasmak0@gmail.com>
2024-05-04 17:27:05 +0200

    Add Redux Toolkit in project

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,14 +1,21 @@
 import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Header from './pages/Header'
 import MainComponent from './pages/MainComponent'
+import Menu from './components/Menu';
 
 function App() {
 
+  const router = createBrowserRouter([{
+    path: '/',
+    element: <Header/>,
+    children:[
+      {path: '/', element:<MainComponent/> },
+      {path: '/menu', element: <Menu/>}
+    ] 
+  }])
   return (
-    <>
-       <Header/>
-       <MainComponent/>
-    </>
+    <RouterProvider router={router}/>
   )
 }
 
--- vite-project/src/pages/Header.tsx
+++ vite-project/src/pages/Header.tsx
@@ -1,11 +1,16 @@
+import { Outlet } from 'react-router-dom';
+
 const Header = () => {
     return(
+        <>
         <nav className="nav-bar">
             <ul>
                 <li>FAST REACT PIZZA CO.</li>
                 <li><input type="text" placeholder=" Search Order  #"/></li>
             </ul>
         </nav>
+        <Outlet/>
+         </>
     )
 }
 export default Header;
