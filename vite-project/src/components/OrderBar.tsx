39471c7000b4c76247febf1ff29bfd160f008114
Sasa Nedic <svetasmak0@gmail.com>
2024-05-06 14:29:37 +0200

    Create OrderBar component

---
+++ vite-project/src/components/OrderBar.tsx
@@ -0,0 +1,13 @@
+const OrderBar = () => {
+    return(
+        <div>
+        <div>
+         <p>Number</p>
+         <p>PIZZAS</p>
+         <p>$24.00</p>
+        </div>
+        <p>OPEN CART </p>
+        </div>
+    )
+}
+export default OrderBar;
