a085a129d932c4dac09cc348c1e58b6afb738543
Sasa Nedic <svetasmak0@gmail.com>
2024-05-08 23:42:47 +0200

    Upload code

--- vite-project/src/components/OrderBar.tsx
+++ vite-project/src/components/OrderBar.tsx
@@ -4,13 +4,14 @@ const OrderBar = () => {
     const pizzaState = useSelector(state => state.pizza)
      console.log('pizzaState', pizzaState)
     const totalPizzas = Object.values(pizzaState).reduce((total, pizza) => total + pizza.count, 0)
-    
-
+    const totalPrice = Object.values(pizzaState).reduce((total,pizza) => total + (pizza.count * pizza.unitPrice), 0)
+    console.log(totalPrice)
+ 
     return(
         <div className="orderbar-container">
          <div className="orderbar-details">
-          <p>{totalPizzas}</p>
-          <p>$24.00</p>
+          <p>{totalPizzas} PIZZAS</p>
+          <p>${totalPrice.toFixed(2)} </p>
          </div>
           <p className="openCart">OPEN CART  </p>
         </div>
