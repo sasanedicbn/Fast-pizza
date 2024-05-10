c6c489a02fb0127cdb947a256675f1456190bd3f
Sasa Nedic <svetasmak0@gmail.com>
2024-05-11 00:34:56 +0200

    FIll order component with data

--- vite-project/src/pages/Order.tsx
+++ vite-project/src/pages/Order.tsx
@@ -1,6 +1,29 @@
 const Order = () => {
     return(
-        <h2>Ready to order? Let's go!</h2>
+        <div className="order-container">
+            <h2>Ready to order? Let's go!</h2>
+            <form>
+                <div>
+                    <label htmlFor="firstName">First Name</label>
+                    <input type="text" id="firstName" />
+                </div>
+                <div>
+                    <label htmlFor="phoneNumber">Phone number</label>
+                    <input type="text" id="phoneNumber" />
+                </div>
+                <div>
+                    <label htmlFor="location">Location</label>
+                    <input type="text" id="location" />
+                    <button>Get Position</button>
+                </div>
+                <div>
+                    <input type="checkbox" id="priorityOrder" />
+                    <label htmlFor="priorityOrder">Want to give your order priority?</label>
+                </div>
+                <button>ORDER NOW FOR PRICE</button>
+            </form>
+        </div>
     )
 }
+
 export default Order;
