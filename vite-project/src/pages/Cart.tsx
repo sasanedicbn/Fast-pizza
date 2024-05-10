d3da6874686a9c6de7d57833f2a9bc1f833e32d4
Sasa Nedic <svetasmak0@gmail.com>
2024-05-10 20:12:06 +0200

    Conditional renreding cart

--- vite-project/src/pages/Cart.tsx
+++ vite-project/src/pages/Cart.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Link } from "react-router-dom";
 import NumberPizza from "../components/NumberPizza";
 import { useDispatch, useSelector } from "react-redux";
@@ -5,35 +6,44 @@ import { addPizza, clearCart, removePizza } from "../store/NumberPizza";
 
 const Cart = () => {
     const orderPizzas = useSelector(state => state.pizza);
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
     console.log(orderPizzas);
+
     function handleClearCart() {
-        dispatch(clearCart())
+        dispatch(clearCart());
     }
-    
-    
+
     return (
         <div>
-                <div  className="cart-container">
-                    <Link to='/menu' className="back-menu">← Back to menu</Link>
-                    <p className="cart-name">Your cart, name</p>
-                    {orderPizzas.map((pizza) => (
-                    <div className="cart-details" key={pizza.id}>
-                        <ul>
-                            <li>{pizza.count}<span>x</span> {pizza.name}</li>
-                        </ul>
-                        <div className="cart-price-details">
-                            <span>${((pizza.count * pizza.unitPrice).toFixed(2))}</span>
-                            <NumberPizza pizzaCount={pizza.count} handleAddPizza={() => dispatch(addPizza(pizza))}
-                            handleRemovePizza={() => dispatch(removePizza(pizza)) }/>
+            <div className="cart-container">
+                <Link to='/menu' className="back-menu">← Back to menu</Link>
+                {orderPizzas.length === 0 ? (
+                    <p className="empy-cart">Your cart is still empty. Start adding some pizzas.</p>
+                ) : (
+                    <>
+                        <p className="cart-name">Your cart, name</p>
+                        {orderPizzas.map((pizza) => (
+                            <div className="cart-details" key={pizza.id}>
+                                <ul>
+                                    <li>{pizza.count}<span>x</span> {pizza.name}</li>
+                                </ul>
+                                <div className="cart-price-details">
+                                    <span>${((pizza.count * pizza.unitPrice).toFixed(2))}</span>
+                                    <NumberPizza 
+                                        pizzaCount={pizza.count} 
+                                        handleAddPizza={() => dispatch(addPizza(pizza))}
+                                        handleRemovePizza={() => dispatch(removePizza(pizza))}
+                                    />
+                                </div>
+                            </div>
+                        ))}
+                        <div>
+                            <button className="custom-button">ORDER PIZZAS</button>
+                            <button className="custom-button clear-btn" onClick={handleClearCart}>CLEAR CART</button>
                         </div>
-                    </div>
-                    ))}
-                    <div>
-                        <button className="custom-button">ORDER PIZZAS</button>
-                        <button className="custom-button clear-btn" onClick={handleClearCart}>CLEAR CART</button>
-                    </div>
-                </div>
+                    </>
+                )}
+            </div>
         </div>
     );
 };
