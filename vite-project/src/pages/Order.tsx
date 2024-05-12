eaa27ce64a59d629c81a44ba78d023264a12612f
Sasa Nedic <svetasmak0@gmail.com>
2024-05-12 14:32:58 +0200

    Add validation

--- vite-project/src/pages/Order.tsx
+++ vite-project/src/pages/Order.tsx
@@ -1,13 +1,18 @@
 import {useForm, SubmitHandler} from 'react-hook-form'
+import {z} from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'; 
 
-type FormFields ={
-    firstName:string,
-    phoneNumber:number,
-    location:string,
-}
+
+const schema =  z.object({
+    firstName:z.string().min(1),
+    phoneNumber:z.string().min(6),
+    location:z.string().min(4),
+})
+type FormFields = z.infer<typeof schema>;
+
 const Order = () => {
   
-    const {register, handleSubmit} = useForm<FormFields>()
+    const {register, handleSubmit, formState: {errors}} = useForm<FormFields>({resolver: zodResolver(schema)})
 
     const onSubmit: SubmitHandler<FormFields> = (data) => {
         console.log(data)
@@ -19,15 +24,18 @@ const Order = () => {
                 <div>
                     <label htmlFor="firstName">First Name</label>
                     <input {...register("firstName")} type="text" id="firstName" />
+                    <span>{errors.firstName?.message}</span>
                 </div>
                 <div>
                     <label htmlFor="phoneNumber">Phone number</label>
                     <input  {...register("phoneNumber")} type="text" id="phoneNumber" />
+                    <span>{errors.phoneNumber?.message}</span>
                 </div>
                 <div>
                     <label htmlFor="location">Location</label>
                     <input  {...register("location")} type="text" id="location" />
                     <button className="btn-position">Get Position</button>
+                    <span>{errors.location?.message}</span>
                 </div>
                 <div>
                     <input type="checkbox" id="priorityOrder" />
