b55ce181a97ff31d23f153c74548e2968aea7083
Sasa Nedic <svetasmak0@gmail.com>
2024-05-16 13:09:46 +0200

    Create changes in Order Status component

--- vite-project/src/pages/OrderStatus.tsx
+++ vite-project/src/pages/OrderStatus.tsx
@@ -1,10 +1,23 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { togglePriority } from '../store/CustomerSlice';
+import { useEffect, useState } from 'react';
 
 const OrderStatus = () => {
+    const [priorityPrice, setPriorityPrice] = useState(0);
     const { id } = useParams();
     const order = useSelector(state => state.pizza.cart[state.pizza.cart.length - 1]);
+    const priority = useSelector(state => state.customer.priority);
+    const dispatch = useDispatch();
+
    
+
+    useEffect(() => {
+        if (order) {
+            const newPriorityPrice = priority ? order.data.orderPrice * 0.05 : 0;
+            setPriorityPrice(newPriorityPrice);
+        }
+    }, [priority, order]);
 
     const formatEstimatedDelivery = (estimatedDelivery) => {
         const date = new Date(estimatedDelivery);
@@ -23,15 +36,21 @@ const OrderStatus = () => {
         const deliveryTime = new Date(estimatedDelivery);
         const differenceInMillis = deliveryTime - now;
         const differenceInMinutes = Math.ceil(differenceInMillis / 60000);
+        if(priority){
+            const speedUpOrder = differenceInMinutes - 7;
+            return speedUpOrder
+        }
         return differenceInMinutes;
     };
+
+    console.log(priority);
 
     return (
         <div className='container-orderStatus'>
             <div className='order-status'>
                 <p>Order #{id} status</p>
                 <div>
-                    <button>PRIORITY</button>
+                    {priority && <button>PRIORITY</button>}
                     <button>PREPARING ORDER</button>
                 </div>
             </div>
@@ -55,12 +74,19 @@ const OrderStatus = () => {
                 {order && (
                     <>
                         <p>Price per pizza: ${order.data.orderPrice}</p>
-                        <p>Price for priority: ${order.data.priorityPrice}</p>
-                        <p>To pay on delivery: ${order.data.orderPrice + order.data.priorityPrice}</p>
+                        {priority && <p>Price for priority: ${priorityPrice.toFixed(2)}</p>}
+                        <p>To pay on delivery: ${(order.data.orderPrice + priorityPrice).toFixed(2)}</p>
                     </>
                 )}
             </div>
-            <button className='order-priority'>MAKE PRIORITY</button>
+            {!priority && (
+                <button 
+                    className='order-priority' 
+                    onClick={() => dispatch(togglePriority(!priority))}
+                >
+                    MAKE PRIORITY
+                </button>
+            )}
         </div>
     );
 };
